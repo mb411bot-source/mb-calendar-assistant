c62ab561e45374170b5ab44d1dff3797d996ac15
mb411bot-source <mb411bot@gmail.com>
2026-09-25 16:48:35 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,4 @@
-export const metadata = { title: 'Moses Brown Calendar Assistant' };
+export const metadata = { title: 'Mo B Calendar Assistant' };
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
